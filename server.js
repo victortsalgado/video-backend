5124c374ecef6b136f1388a3a1836d7d02999160
Victor Santos <58259584+victortsalgado@users.noreply.github.com>
2024-12-25 21:17:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,12 @@ const app = express();
 
 app.use(express.json());
 
-// Rota inicial (verificar se o backend está funcionando)
+// Rota inicial (raiz)
 app.get("/", (req, res) => {
     res.send("Backend está funcionando!");
-});
-
-// Rota para processar vídeo
-app.post("/process-video", (req, res) => {
-    const { videoUrl } = req.body; // Recebe o link do vídeo
-    res.send(`Processando o vídeo: ${videoUrl}`);
 });
 
 // Inicializa o servidor
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+
